1caec2c9f8f6b36f1f4798e7ac8af2f661b9af28
VladimirBalin <vladimirzq@gmail.com>
2020-01-09 16:44:06 +0600

    fixed something

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,25 @@
-'use strict';
+'use strict'
 
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD");
-let timeData = time;
-let income = [];
-let expenses = {};
-let optionalExpenses = {};
-
+let money = prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = { 
-    money,
-    timeData,
-    expenses,
-    optionalExpenses,
-    income,
-    saving: "",
+    budget: money,
+    timeData : time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false,
 };
-let first = prompt("Введите обязательную статью расходов в этом месяце", "");
-let second = prompt("Во сколько обойдется?", "");
-expenses = { first : second }
 
-alert(money/30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    appData.expenses[a] = b;
+}
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);

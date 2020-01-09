9d4fbe760a5cd99f8940dd69edb1c1cef2f4d47c
VladimirBalin <vladimirzq@gmail.com>
2020-01-09 19:12:18 +0600

    thirdtask

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ""),
+let money = +prompt("Ваш бюджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = { 
@@ -15,8 +15,34 @@ let appData = {
 for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
         b = prompt("Во сколько обойдется?", "");
+    
+    if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null 
+        && a != '' && b != '' && a.length < 50) {
 
-    appData.expenses[a] = b;
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+    else {
+        i--;
+    }    
+};
+
+console.log(appData.expenses);
+
+appData.moneyPerDay = appData.budget / 30; 
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 300) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 300 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошло непредвиденное");
 }
 
-alert(appData.budget / 30);
+
+
+ 
